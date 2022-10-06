79a192a01842f28d18fb68fed7a5d1b9ce597540
Srivignesh Sivakumar <shreez77890@gmail.com>
2022-10-06 17:07:19 -0500

    Node Exercise

---
+++ routes/users.route.js
@@ -0,0 +1,47 @@
+import express from "express";
+import db from "../mockdb";
+
+const router = express.Router();
+
+router.get("/id?", async (req, res, next) => {
+    try {
+        let { id } = req.params;
+        let data;
+
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.post("/", async (req, res, next) => {
+    try {
+    let newUser = req.body;
+    let data = await db.add(newUser);
+    res.json(data);
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.put("/:id?", (req, res, next) => {
+    try {
+let { id } = req.params.id;
+let updatedUser = req.body;
+let data = await db.update(id, updatedUser);
+res.json(data);
+     } catch (err) {
+        next(err);
+}
+});
+
+router.delete("/", async (req, res, next) => {
+    try {
+      let { id } = req.params;
+      let data = await db.remove(id);
+      res.json(data);
+    } catch (err) {
+        next(err);
+    }
+});
+
+export default router;
